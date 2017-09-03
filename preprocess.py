ba3dc5541542a47262ff40bf7e83f3da8afc3da0
ShouldChan <ezanmydream@163.com>
2017-09-03 21:43:38 +0800

    preprocess_0943

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,6 @@
 # coding:utf-8
 import time
 
-
 # step1.对用户和POI重新编号
 user_dic = {}
 user_set = set()
@@ -17,18 +16,50 @@ with open('./data.txt','rb') as fread:
 		poi_set.add(poiid)
 	user_list = list(set(user_set))
 	poi_list = list(set(poi_set))
-	print user_list, poi_list
+	# print user_list, poi_list
 	# user_num: 606  poi_num: 2465
 	print len(user_list), len(poi_list)
 
 user_count = 1
 for i in user_list:
-	user_dic[user_count] = i
+	user_dic[i] = user_count
 	user_count += 1
 print user_dic
 
 poi_count = 1
 for j in poi_list:
-	poi_dic[poi_count] = j
+	poi_dic[j] = poi_count
 	poi_count += 1
 print poi_dic
+
+# step2.对数据集按新的编号写入
+from datetime import datetime, timedelta
+
+dataset = []
+
+with open('./data.txt','rb') as fread:
+	lines = fread.readlines()
+	for line in lines:
+		temp = line.strip().split('\t')
+		userid, poiid, timestamp = temp[0], temp[1], temp[2]
+		userid = user_dic[userid]
+		poiid = poi_dic[poiid]
+		# ts = datetime.strptime(str(timestamp), '%Y-%m-%d %H:%M:%S')
+		print userid, poiid, timestamp
+		dataset.append([userid, poiid, timestamp])
+		# fwrite.write()
+
+import operator
+dataset.sort(key=operator.itemgetter(0,2))
+
+# print dataset
+
+with open('./data_v1.txt', 'wb') as fwrite:
+	for i in range(len(dataset)-1):
+		temp_cu = dataset[i]
+		temp_nx = dataset[i+1]
+		userid_cu, poiid_cu, timestamp_cu = temp_cu[0], temp_cu[1], temp_cu[2]
+		userid_nx, poiid_nx, timestamp_nx = temp_nx[0], temp_nx[1], temp_nx[2]
+		if userid_cu == userid_nx:
+			print userid_cu, poiid_cu, timestamp_cu, timestamp_nx
+
