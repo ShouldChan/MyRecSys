17443d125630018ff8ffbe6e0a7c0a249ba44842
ShouldChan <ezanmydream@163.com>
2017-09-26 20:38:43 +0800

    icde_prme_ndcg_2038

--- icde_prme_6_criteria.py
+++ icde_prme_6_criteria.py
@@ -1,6 +1,7 @@
 import time
+import math
 
-n_users = int(354)
+n_users = int(762)
 
 def calculate(topk):
     # read list
@@ -53,14 +54,47 @@ def calculate(topk):
 
         MAP = mean_average_precision(newTestData,newTopData)
         print 'MAP:  %f' % MAP
-        
-        # with open('./result/next_acc.txt', 'a+') as fwrite:
+
+        # with open('./result/acc_map_ndcg.txt', 'a+') as fwrite:
         #     fwrite.write('top'+str(K)+'\taccuracy: '+str(accuracy) \
-        #         + str(recall)+'\n')
-        
-        
+        #         +'\tMAP: '+str(MAP)+'\n')
+        ndcg = getNDCG(newTestData,newTopData)
+        print 'ndcg: %f' % ndcg
 
+def getDCG(rels):
+    dcg = rels[0]
+    i = 2
+    for rel in rels[1:]:
+        dcg = dcg + pow(2,rel) / math.log(i,2)
+        i += 1
+    return dcg
 
+def getIDCG(rels):
+    rels.sort()
+    rels.reverse()
+    return getDCG(rels)
+
+def getNDCG(testData, topData):
+    # followed the loop, len(topdata[i]) is [1,5,10,15,20]
+    # objective: to mark each pid of topData[i](compared with corresponding testData) in three levels
+    # to perform the ranking criteria better
+    scores = []
+    ndcg = 0.0
+    dcg = 0.0
+    idcg = 0.0
+    for i in range(len(topData)):
+        for j in topData[i]:
+            if j in testData[i]:
+                scores.append(1)
+            else:
+                scores.append(0)
+        # print scores
+        dcg += getDCG(scores)
+        idcg += getIDCG(scores)
+        scores = []
+    ndcg = dcg/idcg
+    return ndcg
+            
 def precision_recall(testData, topData, K):
     precision, recall = 0.0, 0.0
     for each_test, each_top in zip(testData, topData):
@@ -82,7 +116,7 @@ def next_acc(testData, topData):
     return acc / n_users
 
 def mean_average_precision(testData, topData):
-    print len(testData[1]), len(topData) # len(testData)==n_users
+    # print len(testData[1]), len(topData) # len(testData)==n_users
     ap = float(0.0)
     for i in range(len(topData)):
         sum = float(0.0)
