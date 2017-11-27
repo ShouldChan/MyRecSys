4190616d08da5e67fa61bf9739f37a4f277ae41b
ShouldChan <ezanmydream@163.com>
2017-11-27 14:38:03 +0800

    homo_youtubeuserprofile

--- mm18_homo_read_sql.py
+++ mm18_homo_read_sql.py
@@ -1,21 +1,27 @@
+# coding:utf-8
 import MySQLdb
 
-conn=MySQLdb.Connection('localhost','root','','homo')
+# first  u should use source *.file commmand to input in mysql database
+conn=MySQLdb.Connection('localhost','root','','test')
 
 cursor=conn.cursor()
 
-sql='select * from googleplususerinfo_tmm'
+sql='select * from youtubeuserinfo_tmm'
 cursor.execute(sql)
 
+# fetchall接收所有的返回结果行
 data=cursor.fetchall()
 
 data=list(data)
 
 import pandas as pd 
-mydata = pd.DataFrame(data,columns=['userid','userName','gender', 'tagLine', \
-    'aboutMe','Organization','birthday','placesLived'])
+mydata = pd.DataFrame(data,columns=['GooglePlusId','username','age', 'gender', \
+    'company','aboutme','hobbies','Hometown','Location','Movies','subscriberCnt','viewcnt','TotalUploadViews'])
 
-print(mydata)     
+# print(mydata)
+# mydata.to_csv('./googleplususerinfo.csv')
+mydata.to_csv('./youtubeuserinfo_withheader.csv',index=False,header=True)
+mydata.to_csv('./youtubeuserinfo_noindex_noheader.csv',index=False,header=False)
 
 # with open('./googleplususerinfo_tmm.txt','w') as fw:
 #     fw.write(str(data))
